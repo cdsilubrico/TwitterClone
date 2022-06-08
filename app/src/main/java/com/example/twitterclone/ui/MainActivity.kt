efbd666b0fb83e351c6263ff9030a9cfe69a9f4b
Clowie Silubrico <clowiesilubrico@gmail.com>
2022-06-08 14:28:14 +0800

    Moved package

---
+++ app/src/main/java/com/example/twitterclone/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.twitterclone.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.twitterclone.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bv_bottom_nav)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view_main) as NavHostFragment
+
+        bottomNavigationView.setupWithNavController(navHostFragment.navController)
+    }
+}
