c2fda2b91fc1b215f37563d5330c9dd5240e7f01
Clowie Silubrico <clowiesilubrico@gmail.com>
2022-06-10 01:23:06 +0800

    Add file

---
+++ app/src/main/java/com/example/twitterClone/ui/activity/Auth.kt
@@ -0,0 +1,13 @@
+package com.example.twitterClone.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.twitterclone.R
+
+class Auth : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+    }
+}
