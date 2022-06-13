b53b5d6df9db5ac8fa0b1900b3720f20db1c963a
Clowie Silubrico <clowiesilubrico@gmail.com>
2022-06-13 14:53:22 +0800

    Update landing fragment

--- app/src/main/java/com/example/twitterClone/ui/fragment/Landing.kt
+++ app/src/main/java/com/example/twitterClone/ui/fragment/Landing.kt
@@ -30,35 +30,59 @@ class Landing : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        landingBinding = FragmentLandingBinding.inflate(inflater,container,false)
+        landingBinding = FragmentLandingBinding.inflate(inflater, container, false)
 
 
         return landingBinding.root
     }
 
-    private fun changeLoginSubStringColor()
-    {
+    private fun changeLoginSubStringColor() {
         val tvText = resources.getString(R.string.label_have_an_account_already)
         val spannableString = SpannableString(tvText)
         val id = resources.getString(R.color.twitter)
-        spannableString.setSpan(ForegroundColorSpan(id.toColorInt()),25,31,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(
+            ForegroundColorSpan(id.toColorInt()),
+            25,
+            31,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
         landingBinding.haveAnAccountAlready.text = spannableString
     }
 
-    private fun changePrivacyTermsSubStringColor()
-    {
+    private fun changePrivacyTermsSubStringColor() {
         val tvText = resources.getString(R.string.label_terms_privacy_policy_cookie)
         val spannableString = SpannableString(tvText)
         val id = resources.getString(R.color.twitter)
-        spannableString.setSpan(ForegroundColorSpan(id.toColorInt()),31,37,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableString.setSpan(ForegroundColorSpan(id.toColorInt()),38,53,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableString.setSpan(ForegroundColorSpan(id.toColorInt()),59,tvText.length-1,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(
+            ForegroundColorSpan(id.toColorInt()),
+            31,
+            37,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            ForegroundColorSpan(id.toColorInt()),
+            38,
+            53,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            ForegroundColorSpan(id.toColorInt()),
+            59,
+            tvText.length - 1,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
         landingBinding.termsPrivacyCookie.text = spannableString
+    }
+
+    private fun setSubStringColor() {
+        changeLoginSubStringColor()
+        changePrivacyTermsSubStringColor()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        changeLoginSubStringColor()
-        changePrivacyTermsSubStringColor()
+        setSubStringColor()
     }
+
+
 }
