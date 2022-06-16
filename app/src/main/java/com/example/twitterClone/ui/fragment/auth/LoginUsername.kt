ac9a2845c0edea229a7e9460f3a65819d79364cb
Clowie Silubrico <clowiesilubrico@gmail.com>
2022-06-16 22:36:15 +0800

    Update class name

--- app/src/main/java/com/example/twitterClone/ui/fragment/auth/LoginUsername.kt
+++ app/src/main/java/com/example/twitterClone/ui/fragment/auth/LoginUsername.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.twitterclone.R
 
-class Login : Fragment() {
+class LoginUsername : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
