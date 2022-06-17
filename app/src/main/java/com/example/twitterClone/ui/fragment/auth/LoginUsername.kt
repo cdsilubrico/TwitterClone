c2c2bb17c57a2646494b5c0ca369d979979a4d6e
Clowie Silubrico <clowiesilubrico@gmail.com>
2022-06-18 03:35:33 +0800

    Pass data to next screen

--- app/src/main/java/com/example/twitterClone/ui/fragment/auth/LoginUsername.kt
+++ app/src/main/java/com/example/twitterClone/ui/fragment/auth/LoginUsername.kt
@@ -5,19 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.twitterclone.R
+import androidx.navigation.fragment.findNavController
+import com.example.twitterclone.databinding.FragmentLoginUsernameBinding
 
 class LoginUsername : Fragment() {
 
+    private lateinit var loginUsernameBinding: FragmentLoginUsernameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initializeString()
+    }
+
+    private fun initializeString() {
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        loginUsernameBinding = FragmentLoginUsernameBinding.inflate(layoutInflater,container,false)
+
+        return loginUsernameBinding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeString()
+        onNextClick()
+    }
+
+    private fun onNextClick() {
+        loginUsernameBinding.btnNext.setOnClickListener{
+            navigatePasswordScreen()
+        }
+    }
+
+    private fun navigatePasswordScreen(){
+        val action = LoginUsernameDirections.actionLoginToLoginPassword(getUsername())
+        findNavController().navigate(action)
+
+    }
+
+    private fun getUsername(): String {
+        return loginUsernameBinding.etPhoneEmailUsername.text.toString()
+    }
+
+
 }
